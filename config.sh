49671a8c1590ef76149eec71538bac215c2576d7
essaNAJJAR <51336462+essaNAJJAR@users.noreply.github.com>
2019-12-30 16:54:44 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=essaNAJJAR
+GITHUB_EMAIL=aramsam11@gmail.com
 
-KBUILD_BUILD_USER="Aki"
+KBUILD_BUILD_USER="EssaNajjar"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=xiaomi
+export device=whyred
 
-ROM="PixelExperience"
+ROM="RR"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
