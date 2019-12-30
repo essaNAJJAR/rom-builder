289aeee72d7141dbd720c8ffe97397b9106990c1
essaNAJJAR <51336462+essaNAJJAR@users.noreply.github.com>
2019-12-30 17:03:39 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ GITHUB_USER=essaNAJJAR
 GITHUB_EMAIL=aramsam11@gmail.com
 
 KBUILD_BUILD_USER="EssaNajjar"
-KBUILD_BUILD_HOST="A_DEAD_PLANET"
+KBUILD_BUILD_HOST="No_one_care"
 
 export oem=xiaomi
 export device=whyred
@@ -12,8 +12,8 @@ export device=whyred
 ROM="RR"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+branch="ten"
 
 release_repo="Akianonymus/harpia_builds"
 
-timezone="Asia/Kolkata"
+timezone="Asia/syria"
